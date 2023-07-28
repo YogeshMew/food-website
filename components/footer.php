57fedadc8407d7a64e2cd2db74234ecc003b54ba
Yogesh <yrmewara@gmail.com>
2023-07-28 16:51:03 +0530

    initial commit

---
+++ components/footer.php
@@ -0,0 +1,38 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <img src="images/email-icon.png" alt="">
+         <h3>our email</h3>
+         <a href="mailto:shreebholenathvp12@gmail.com">shreebholenathvp12@gmail.com</a>
+      </div>
+
+      <div class="box">
+         <img src="images/clock-icon.png" alt="">
+         <h3>opening hours</h3>
+         <p>11:00am to 11:00pm</p>
+      </div>
+
+      <div class="box">
+         <img src="images/map-icon.png" alt="">
+         <h3>our address</h3>
+         <a href="#">Hotel Shree Bholanath, Akshay Apartments, Subhash Rd, Opp. New, Vishnu Prasad Society, Navpada, Vile Parle East, Vile Parle, Mumbai, Maharashtra 400057, India</a>
+      </div>
+
+      <div class="box">
+         <img src="images/phone-icon.png" alt="">
+         <h3>our number</h3>
+         <a href="tel:1234567890">81049 58289</a>
+         <a href="tel:1234567890"><a href="tel:1234567890">098197 42615</a></a>
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>mr. web designer</span> | all rights reserved!</div>
+
+</footer>
+
+<div class="loader">
+   <img src="images/loader.gif" alt="">
+</div>
